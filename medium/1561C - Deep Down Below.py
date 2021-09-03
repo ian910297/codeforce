fa85b6ad68fa2ac82335b19e461323adc667638f
Demonic <demonic@csie.io>
2021-09-03 23:36:40 +0800

    incorrect on test 2

--- medium/1561C - Deep Down Below.py
+++ medium/1561C - Deep Down Below.py
@@ -9,72 +9,42 @@ def main():
             cave_info.append([int(x) for x in input().split(" ")])
             cave_info[i] = cave_info[i][1:]
 
-        # sorted_info = []
+        sorted_info = []
         # # find maximal value by each cave
         # # float('inf')
         # # float('-inf')
-        # for i in range(caves):
-        #     maxi_monster_value = cave_info[i][0]
-        #     maxi_monster_idx = 0
-        #     for j in range(1, len(cave_info[i])):
-        #         if maxi_monster_value < cave_info[i][j]:
-        #             maxi_monster_value = cave_info[i][j]
-        #             maxi_monster_idx = j
+        for i in range(caves):
+            tmp_answer = cave_info[i][0] + 1
+            tmp_power = tmp_answer
+            for j in range(1, len(cave_info[i])):
+                tmp_power += 1
+                if tmp_power < cave_info[i][j]:
+                    tmp_answer = cave_info[i][j] + 1 - j
+                    tmp_power = cave_info[i][j] + 1
         
-        #     sorted_info.append((maxi_monster_value, maxi_monster_idx, i))
+            sorted_info.append((tmp_answer, i))
 
-        # from operator import itemgetter, attrgetter
+        from operator import itemgetter, attrgetter
         # # sorted(student_tuples, key=itemgetter(2))
         # # sorted(student_tuples, key=itemgetter(1,2))
         # # sorted(student_tuples, key=itemgetter(2), reverse=True)
         # # [('john', 'A', 15), ('dave', 'B', 10), ('jane', 'B', 12)]
         # # sorted(student_objects, key=attrgetter('age'))
-        # sorted_info = sorted(sorted_info, key=itemgetter(0), reverse=True)
+        sorted_info = sorted(sorted_info, key=itemgetter(0))
 
-        visited_cave = []
-        answer = None
-        power = None
-        while len(visited_cave) < caves:
-            candidate_idx = None
-            candidate_answer = None
-            candidate_power = None
-            for cave_idx in range(caves):
-                if cave_idx in visited_cave:
-                    continue
-                
-                # first round
-                if answer == None:
-                    current_answer = None
-                    current_power = None
+        for i in range(caves):
+            if i == 0:
+                answer = sorted_info[0][0]
+                power = sorted_info[0][0]
+                steps = len(cave_info[sorted_info[0][1]])
+            else:
+                steps += len(cave_info[sorted_info[i - 1][1]])
+                if power + len(cave_info[sorted_info[i - 1][1]]) < sorted_info[i][0]:
+                    diff = sorted_info[i][0] - (power + len(cave_info[sorted_info[i - 1][1]]))
+                    answer += diff
+                    power = sorted_info[i][0]
                 else:
-                    current_answer = answer
-                    current_power = power
-
-                # walk
-                for monster_idx in range(0, len(cave_info[cave_idx])):
-                    if current_answer == None:
-                        current_answer = cave_info[cave_idx][monster_idx] + 1
-                        current_power = current_answer + 1
-                        continue
-
-                    if current_power <= cave_info[cave_idx][monster_idx]:
-                        # print("current_answer({}) = current_power({}) - current_steps({})".format(current_answer, current_power, current_steps))
-                        current_answer -= current_power
-                        current_power = cave_info[cave_idx][monster_idx] + 1
-                        current_answer += current_power
-
-                    current_power += 1
-
-                # store info
-                if candidate_idx == None or candidate_answer > current_answer:
-                    # print("store", cave_idx, current_answer, current_power)
-                    candidate_idx = cave_idx
-                    candidate_answer = current_answer
-                    candidate_power = current_power
-            visited_cave.append(candidate_idx)
-            # print("answer", candidate_answer, current_steps)
-            answer = candidate_answer
-            power = candidate_power
+                    power += steps
 
         print(answer)
         times -= 1
